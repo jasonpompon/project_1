7f6848541760620f1cf9d8157cea8a38b58c8b9d
Jason Pompon <jasonpompon@gmail.com>
2016-10-13 00:26:14 +1100

    some styling improvements

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
 
 # project_1
-
-
-<!-- *** this is how to do a nested loop to put some string in a nested array *** -->
-
-function makeArray(x, y) {
-  var array = [];
-  for(var i = 0; i < y; i++){
-    var tempRow = [];
-    for(var j = 0; j < x; j++){
-      tempRow[j] = "A";
-    }
-    array[i] = tempRow;
-  }
-  return array;
-}
-
-
-console.log(makeArray(5,5));
--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,5 @@
 // writing variables on top
 
-
 var input = [
   [00,01,02],
   [10,11,12],
@@ -8,59 +7,72 @@ var input = [
 ];
 
 var playerTurn = 0;
-
 var sign = '';
+var gameState = true;
 
 // variance to link each div from DOM
 var boardDiv = document.getElementById('board');
+var result = document.getElementById('resultDiv');
 
-console.log(input);
 
+// function to determine who won
 function ticTacToe() {
-  // declare a winner when 3
+  // declare a winner when 3cosecutive sign is found
   if (input[0][0] === input[1][1] && input[1][1] === input[2][2]) {
-    console.log('WINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
   } else if(input[0][2] === input[1][1] && input[1][1] === input[2][0]) {
-    console.log('WINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
   } else if(input[0][0] === input[1][0] && input[1][0] === input[2][0]) {
-    console.log('WINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
   } else if(input[0][1] === input[1][1] && input[1][1] === input[2][1]) {
-    console.log('WINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
   } else if(input[0][2] === input[1][2] && input[1][2] === input[2][2]) {
-    console.log('WINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
   } else if(input[0][0] === input[0][1] && input[0][1] === input[0][2]) {
-    console.log('WINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
   } else if(input[1][0] === input[1][1] && input[1][1] === input[1][2]) {
-    console.log('WINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
   } else if(input[2][0] === input[2][1] && input[2][1] === input[2][2]) {
-    console.log('WINNER');
-  } else {
-    console.log('NOWINNER');
+    result.textContent = sign + ' is the WINNER!';
+    gameState = false;
+  } else if (playerTurn >= 9)  {
+    result.textContent = 'DRAW!';
+    gameState = false;
   }
 }
 
 
 
-
+// adding an even listener on the board div then using event.target to target individual divs in the playing area
 boardDiv.addEventListener('click', function(event) {
-  if (event.target.tagName === 'DIV' ) {
-    playerTurn ++;
-    switchPlay();
-    if (sign === 'X') {
-      event.target.style.backgroundImage= 'url("images/Xicon.png")';
-    } else {
-      event.target.style.backgroundImage= 'url("images/O.png")';
+  if (gameState = true) {
+    if (event.target.className !== 'x' && event.target.className !== 'o') {
+      if (event.target.tagName === 'DIV') {
+      playerTurn ++;
+      switchPlay();
+      if (sign === 'X') {
+        event.target.className = 'x';
+      } else {
+        event.target.className = 'o';
+      }
+      var dataRow = parseInt( event.target.getAttribute('data-row'), 10);
+      var dataCol = parseInt( event.target.getAttribute('data-column'), 10);
+      input[dataRow][dataCol] = sign;
+      ticTacToe();
     }
-    var dataRow = parseInt( event.target.getAttribute('data-row'), 10);
-    var dataCol = parseInt( event.target.getAttribute('data-column'), 10);
-    input[dataRow][dataCol] = sign;
-    ticTacToe();
+   }
   }
 });
 
 
-
-
+// function to alternate between X and O
 function switchPlay() {
   if (playerTurn%2 !== 0) {
     sign = 'X';
@@ -68,3 +80,17 @@ function switchPlay() {
     sign = 'O';
   }
 }
+
+// *** gameState not working properly??????? ***
+
+// **** restart button ****
+// when clicked on:
+// 1) change all div's class to none so that all the images goes away
+// reset the input array
+// reset playerTurn to 0
+// reset sign to ''
+// reset gameState to true
+// empty resultDiv
+
+// *** need a 2 variable to keep score of x and o wins ***
+// then display it on the screen
